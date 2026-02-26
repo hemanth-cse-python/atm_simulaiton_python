4070ee0fad67a540c3182c0b31edb1f5a3125cc5
hemanth-cse-python <hemanthbunny023@gmail.com>
2026-02-26 11:38:40 +0530

    Add files via upload

---
+++ atm_simulation.py
@@ -0,0 +1,24 @@
+#atm simmulation
+pin = 1234
+balance = 10000
+enter=int(input('pin: '))
+if enter == pin:
+    print('''1.withdraw
+2.deposite
+3.check balance''')
+    option= int(input('select option'))
+    if option == 1:
+        atm = float(input('enter amount'))
+        if atm<=balance:
+            balance-=atm
+            print('remaning:',balance)
+        else:
+            print('insufficent')
+    elif option == 2:
+        atm = float(input('enter amount'))
+        atm+=balance
+        print('avalilable: ',atm)
+    elif option == 3:
+        print(balance)
+else:
+    print('wrong pin')
